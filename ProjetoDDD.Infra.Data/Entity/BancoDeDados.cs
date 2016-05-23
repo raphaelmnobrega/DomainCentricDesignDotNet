4124384de2f3bb12a22482cfdf3cac4a6c49c11b
Phederal <phederal@lti.sistemas>
2016-05-23 20:02:59 -0300

    Adding infra layer

---
+++ ProjetoDDD.Infra.Data/Entity/BancoDeDados.cs
@@ -0,0 +1,21 @@
+﻿using ProjetoDDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoDDD.Infra.Data.Entity
+{
+    public class BancoDeDados : DbContext
+    {
+
+        public BancoDeDados() : base("ProjetoDDD_DataBase")
+        {
+                
+        }
+
+        DbSet<Cliente> Clientes { get; set; }
+    }
+}
